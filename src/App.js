680bbf4a4cae58111ced52cc0fe2189675a3d33a
YounesAmor <younes1396@gmail.com>
2023-01-31 14:52:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Chart from "./components/char";
+function App() {
+  return (
+    <div className="App bg-cream h-screen w-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        <Chart />
+      </div>
+    </div>
+  );
+}
+
+export default App;
